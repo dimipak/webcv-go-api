02df76318584ee9015a48d52353f5f2048ec6927
dimipak <jimpak13@gmail.com>
2022-04-20 17:05:39 +0300

    admin persmissions

--- machine/bootstrap/after_install.sh
+++ machine/bootstrap/after_install.sh
@@ -3,3 +3,5 @@
 sudo aws s3 cp s3://webcv-configurations/backend/go/.env.production /home/ubuntu/admin/.env
 
 sudo aws s3 cp s3://webcv-configurations/backend/go/dist/admin /home/ubuntu/admin/admin
+
+sudo chmod 755 admin
